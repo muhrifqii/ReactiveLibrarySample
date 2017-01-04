a3dec2043bda9d2ce43d4a077c6073b6c67d6edb
muhrifqii <muh_rif@live.com>
2017-01-04 23:57:48 +0700

    Simple rxbinding view click listener

--- app/src/main/java/io/github/muhrifqii/reactivelibrarysample/MainActivity.java
+++ app/src/main/java/io/github/muhrifqii/reactivelibrarysample/MainActivity.java
@@ -2,12 +2,41 @@ package io.github.muhrifqii.reactivelibrarysample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.jakewharton.rxbinding.view.RxView;
+import rx.subscriptions.CompositeSubscription;
 
 public class MainActivity extends AppCompatActivity {
+
+  final CompositeSubscription subs = new CompositeSubscription();
+  private TextView textView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    textView = (TextView) findViewById(R.id.tv_hello_reactive);
+  }
 
+  @Override protected void onStart() {
+    super.onStart();
+
+    subs.add(RxView.clicks(textView)
+        .subscribe(
+            __ -> Toast.makeText(MainActivity.this, "I am ready!", Toast.LENGTH_SHORT).show()));
+    subs.add(RxView.longClicks(textView)
+        .subscribe(
+            __ -> Toast.makeText(MainActivity.this, "Long click happened", Toast.LENGTH_SHORT)
+                .show()));
+  }
+
+  @Override protected void onStop() {
+    super.onStop();
+    subs.unsubscribe();
+  }
+
+  @Override protected void onDestroy() {
+    subs.clear();
+    super.onDestroy();
   }
 }
